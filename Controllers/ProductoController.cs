7420dbbba1293f77e3ac795cd3582132a4c47a0a
Noel Miño Herrera <noel.minoh@upr.edu.cu>
2022-12-08 16:28:54 -0500

    GetProduct finished

--- Controllers/ProductoController.cs
+++ Controllers/ProductoController.cs
@@ -21,8 +21,8 @@ namespace API_Core.Controllers
         }
 
         [HttpGet]
-        [Route("Lista")]
-        public IActionResult ListProducts() 
+        [Route("ListProducts")]
+        public IActionResult GetListProducts() 
         {
             List<Producto> listProducts = new List<Producto>();
             try
@@ -30,7 +30,7 @@ namespace API_Core.Controllers
                 using (var conn = new SqlConnection(SQLString))
                 {
                     conn.Open();
-                    var cmd = new SqlCommand("[sp_lista_productos]", conn);
+                    var cmd = new SqlCommand("sp_lista_productos", conn);
                     cmd.CommandType = CommandType.StoredProcedure;
 
                     using (var reader = cmd.ExecuteReader())
@@ -55,6 +55,50 @@ namespace API_Core.Controllers
             catch (Exception ex)
             {
                 return StatusCode(StatusCodes.Status500InternalServerError, new { message = ex.Message, response = listProducts });
+            }
+        }
+
+        [HttpGet]
+        [Route("Product/{id:int}")]
+        public IActionResult GetProduct(int id)
+        {
+            try
+            {
+                var product = new Producto();
+
+                using (var conn = new SqlConnection(SQLString))
+                {
+                    conn.Open();
+                    var cmd = new SqlCommand("sp_devuelve_producto", conn);
+                    cmd.Parameters.AddWithValue("IdProducto", id);
+                    cmd.CommandType = CommandType.StoredProcedure;
+
+                    using (var reader = cmd.ExecuteReader())
+                    {
+                        if (reader.Read())
+                        {
+                            product = new Producto()
+                            {
+                                IdProducto = Convert.ToInt32(reader["IdProducto"]),
+                                CodigoBarra = reader["CodigoBarra"].ToString(),
+                                Nombre = reader["Nombre"].ToString(),
+                                Marca = reader["Marca"].ToString(),
+                                Categoria = reader["Categoria"].ToString(),
+                                Precio = Convert.ToDecimal(reader["Precio"])
+                            };
+                        }
+                        else
+                        {
+                            return StatusCode(StatusCodes.Status404NotFound, new { message = "Not Item Found", response = product });
+                        }
+
+                    }
+                }
+                return StatusCode(StatusCodes.Status200OK, new { message = "ok", response = product });
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, new { message = ex.Message, response = ex.InnerException.Message });
             }
         }
     }
